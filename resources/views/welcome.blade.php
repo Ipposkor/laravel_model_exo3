0836cf7e12d050a5a2cdc82715aa7a0a48c95516
Ipposkor <102771791+Ipposkor@users.noreply.github.com>
2022-09-08 14:43:49 +0200

    ajout de tous les compteurs dans chaque section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,23 @@
         <li><a href="#noCondition">Less than 18 & more than 24</a></li>
     </ul>
 </div>
-<h1 class="text-4xl text-center">Number of Members : {{$users->count()}}</h1>
+<h1 class="text-4xl text-center">Number of Members : {{ $users->count() }}</h1>
 @section('content')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 " id="everybody">Everybody</h1>
     @include('partials.allMembers')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 text-blue-600 " id="allMen">Men Only</h1>
+    <h1 class="text-4xl text-center mb-5">Number of Members : {{ $mens->count() }}</h1>
     @include('partials.allMen')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 text-pink-600 " id="allWomen">Women Only</h1>
+    <h1 class="text-4xl text-center mb-5">Number of Members : {{ $womens->count() }}</h1>
     @include('partials.allWomen')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 text-blue-600 " id="menFit">Men (18 -> 24)</h1>
+    <h1 class="text-4xl text-center mb-5">Number of Members : {{ $mensfit->count() }}</h1>
     @include('partials.allMenFit')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 text-pink-600 " id="womenFit">Women (18 -> 24)</h1>
+    <h1 class="text-4xl text-center mb-5">Number of Members : {{ $womensfit->count() }}</h1>
     @include('partials.allWomenFit')
     <h1 class="flex justify-center text-4xl mt-5 mb-5 " id="noCondition">Less than 18 & more than 24</h1>
+    <h1 class="text-4xl text-center mb-5">Number of Members : {{ $nofit->count() }}</h1>
     @include('partials.noCondition')
 @endsection
