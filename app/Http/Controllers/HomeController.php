29379ada971a0f147a6daedf875823d768999eb1
Ipposkor <102771791+Ipposkor@users.noreply.github.com>
2022-09-08 14:25:40 +0200

    fin avec les variables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,11 @@ class HomeController extends Controller
 {
     public function index(){
         $users = DB::table('membres')->get();
-        return view('welcome', compact('users'));
+        $mens= DB::table('membres')->where('genre' , "Homme")->get();
+        $womens= DB::table('membres')->where('genre' , "Femme")->get();
+        $mensfit= DB::table('membres')->where('genre' , "Homme")->whereBetween('age',[18,24])->get();
+        $womensfit= DB::table('membres')->where('genre' , "Femme")->whereBetween('age',[18,24])->get();
+        $nofit= DB::table('membres')->where('age','<',18)->orWhere('age','>',24)->get();
+        return view('welcome', compact('users','mens','womens','mensfit','womensfit','nofit'));
     }
 }
